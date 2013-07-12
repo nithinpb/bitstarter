4b5bbe2c6147a9884c2187a0c5462c548bd2e24f
Nithin Betegeri <nithin@cmu.edu>
2013-07-12 01:08:22 +0000

    Fixed fs import

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var fs = require('fs');
 app.get('/', function(request, response) {
     fs.readFileSync('index.html', function(err, data){
 	if (err) throw err;
-	response.send(data);
+	response.send(new Buffer(data, 'utf8'));
     });
 });
 
